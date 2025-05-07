75b37f2fee5daacfe678f217b602345eb7972319
AlexAmenabar <alejo.amenabar@gmail.com>
2025-05-07 16:21:48 +0200

    Escena lobby y GUI empezados

---
+++ Assets/Lobby/Scripts/GeneralMiniGameController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralMiniGameController : MonoBehaviour
+{
+
+    private int score;
+    private int timer; // seconds
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /* Loads minigame scenes / prefabs into the game scene */
+    void LoadMinigames()
+    {
+
+    }
+    
+    // Initializes all necessary data when staring the game
+    void InitializeGame()
+    {
+
+    }
+
+}
---
+++ Assets/Lobby/Scripts/Minigame.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minigame : MonoBehaviour
+{
+
+    private int minigame_id;
+    private int difficulty;
+    
+
+    void InitializeMinigame()
+    {
+
+    }
+
+    void LoadMinigame()
+    {
+
+    }
+
+    void RestartMinigame()
+    {
+
+    }
+
+    void EndMinigame()
+    {
+
+    }
+}
